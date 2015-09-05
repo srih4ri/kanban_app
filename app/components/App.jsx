7d4b9423e1da2837b6bb274c7ed3d285a6eb1b07
Srihari K <srih4ri@gmail.com>
2015-09-05 20:20:57 +0530

    Add note button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,30 +2,48 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx'
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn Flux'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn Everything'
-  }
-];
-
-
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn Flux'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn Everything'
+        }
+      ]
+    };
+
+    this.addNote = this.addNote.bind(this);
+  }
 
   render () {
     return (
       <div>
-        <Notes items={notes} />
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes items={this.state.notes} />
       </div>
     );
+  }
+
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([
+        {
+          id: uuid.v4(),
+          task: 'Learn Everything'
+        }
+      ])
+    });
   }
 
 }
