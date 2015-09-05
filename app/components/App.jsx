234c8a0e866884c58ad1caf0ce4362c63b3c4c53
Srihari K <srih4ri@gmail.com>
2015-09-05 19:55:02 +0530

    Move note rendering to Notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,31 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx'
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn Webpack'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Learn Flux'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Learn Everything'
+  }
+];
+
 
 export default class App extends React.Component {
+
   render () {
-    return <Note />;
+    return (
+      <div>
+        <Notes items={notes} />
+      </div>
+    );
   }
+
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Note extends React.Component {
   render () {
-    return <p> Learning Webpack </p>;
+    return <p> {this.props.task}</p>;
   }
 }
